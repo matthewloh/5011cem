4b1bf84033f191644bbf3f87c24b85d849f8efde
Matthew Loh <matthewloh256@gmail.com>
2024-04-24 15:06:58 +0800

    no store

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,6 +1,8 @@
 import { createClient } from "@/utils/supabase/server";
+import { unstable_noStore } from "next/cache";
 
 export default async function Notes() {
+  unstable_noStore()
   const supabase = createClient();
   const { data: notes } = await supabase.from("notes").select();
 
